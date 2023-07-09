e83f7e715883be9b20bb4d615c0005a16b0b7cd5
carmukyo <carlmike.aguila@gmail.com>
2023-07-09 20:11:28 +0800

    Add function that allow player to put X O X and a function to alternate between the two

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const gameboard = function(){
-    let board = ["x", "o", "x", "", "", "", "", "", ""];
+const gameBoard = (function(){
+    let board = ["", "", "", "", "", "", "", "", ""];
     const getBoard = function(){
         return board;
     }
@@ -10,12 +10,12 @@ const gameboard = function(){
         board = ["", "", "", "", "", "", "", "", ""];
     }
     return {getBoard, setBoard, resetBoard};
-}();
+})();
 
 
 const displayController = function(){
     const renderBoard = function(){
-        const board = gameboard.getBoard();
+        const board = gameBoard.getBoard();
         const cells = document.querySelectorAll(".cell");
 
         cells.forEach(function(cell,index){
@@ -26,3 +26,41 @@ const displayController = function(){
 }();
 
 displayController.renderBoard();
+
+const player = function(name, marker){
+    return {name, marker};
+}
+
+const gameController = function(){
+    let player1 = player("Player 1", "X");
+    let player2 = player("Player 2", "O");
+    let activePlayer = player1;
+
+    const switchPlayer = function(){
+        if(activePlayer === player1){
+            activePlayer = player2;
+        }else{
+            activePlayer = player1;
+        }
+    }
+
+    const playTurn = function(index){
+        let board = gameBoard.getBoard();
+        gameBoard.setBoard(index, activePlayer.marker);
+        displayController.renderBoard();
+        switchPlayer();
+    }
+
+    const startGame = () => {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell, index) => {
+          cell.addEventListener("click", () => playTurn(index));
+        });
+        displayController.renderBoard();
+      }
+      
+      return { startGame };
+}();
+
+
+gameController.startGame();
